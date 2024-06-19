07f6b499c97641dd944005335dac450a8e0613f2
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-06-19 11:18:12 +0530

    NAVBAR MAIN

--- admin/index.php
+++ admin/index.php
@@ -14,13 +14,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Management System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <!-- Navbar -->
-   <?php include("../LoginPage/navbar.php"); ?>
-   <button class="btn btn-danger" > <a href="../logout.php">Logout</a> </button>
+     <div >
+    <?php include("../LoginPage/navbar.php"); ?>
+    </div>
+     <?php include("../sidebar.php"); ?>
 </body>
 </html>
---
+++ sidebar.php
@@ -0,0 +1,21 @@
+
+<div id="nav-bar">
+  <input id="nav-toggle" type="checkbox"/>
+  <div id="nav-header"><a id="nav-title" href="index.php" ><i class="fas fa-home"></i>HOME</a>
+    <label for="nav-toggle"><span id="nav-toggle-burger"></span></label>
+    <hr/>
+  </div>
+  <div id="nav-content">
+    <div class="nav-button"><i class="fas fa-palette"></i><span>Your Work</span></div>
+    <div class="nav-button"><i class="fas fa-images"></i><span>Assets</span></div>
+    <div class="nav-button"><i class="fas fa-thumbtack"></i><span>Pinned Items</span></div>
+    <div class="nav-button"><i class="fas fa-heart"></i><span>Following</span></div>
+    <div class="nav-button"><i class="fas fa-chart-line"></i><span>Trending</span></div>
+    <div class="nav-button"><i class="fas fa-fire"></i><span>Challenges</span></div>
+    <div class="nav-button"><i class="fas fa-magic"></i><span>Spark</span></div>
+    <hr/>
+    <div class="nav-button"><i class="fas fa-gem"></i><span>Codepen Pro</span></div>
+    <div id="nav-content-highlight"></div>
+  </div>
+  
+</div>
