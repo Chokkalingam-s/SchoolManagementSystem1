173c886a1cb7a45ca8d1a58d1d6ede346ffe27b0
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-06-25 13:51:41 +0530

    hclass

--- teacher/handle_class.php
+++ teacher/handle_class.php
@@ -6,14 +6,6 @@
 		echo"<script>window.open('teacher_login.php?mes=Access Denied...','_self');</script>";
 		
 	}	
-
-    $sql="SELECT * FROM staff WHERE TID={$_SESSION["TID"]}";
-		$res=$db->query($sql);
-
-		if($res->num_rows>0)
-		{
-			$row=$res->fetch_assoc();
-		}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,7 +21,6 @@
 </head>
 
 <body>
-
     <!-- Navbar -->
     <div  style="position: fixed;  width:100%;z-index: 10;">
         <?php include("../LoginPage/navbar.php"); ?>
@@ -38,13 +29,9 @@
         <?php include("../sidebar.php"); ?>
     </div>
     <div class="section">
-
-        <h3 class="text">Welcome <?php echo $_SESSION["TNAME"]; ?></h3><hr><br>
-     
+        <h3 class="text">Welcome <?php echo $_SESSION["TNAME"]; ?></h3><hr>
         <div class="content">
-				
-					<h3>Add Classes</h3><br>
-					
+					<h3>Add Classes</h3><br>	
 					<div class="lbox1">
 					<?php
 						if(isset($_POST["submit"]))
@@ -52,77 +39,51 @@
 							 $sq="insert into hclass(TID,CLA,SEC,SUB) values ('{$_SESSION["TID"]}','{$_POST["cla"]}','{$_POST["sec"]}','{$_POST["sub"]}')";
 							if($db->query($sq))
 							{
-								echo "<div class='success'>Insert Success..</div>";
+								echo "<div class='success'>Insert Success..<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' onclick='closeAlert(this)'></button> </div>";
 							}
 							else
 							{
-								echo "<div class='error'>Insert Failed..</div>";
-							}
-		
-						}
-					
-					
-					?>					
+								echo "<div class='error'>Insert Failed..<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' onclick='closeAlert(this)'></button> </div>";
+                            						}						}
+					?>							
+					<form  method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">				
+					<label>Class / Section / Subject</label><br><br>
 						
-						
-					<form  method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
-					
-					<label>Class</label><br>
-						
-						<select name="cla" required class="input2">
+						<select name="cla" required class="input2" style="width:33%">
 							<?php
 								$sl="select DISTINCT(CNAME) from class";
 								$r=$db->query($sl);
 								 if($r->num_rows>0)
 								 {
-									 echo "<option value=''>Select</option>";
+									 echo "<option value=''>Class</option>";
 									 while($ro=$r->fetch_assoc())
 									 {
 										 echo "<option value='{$ro["CNAME"]}'>{$ro["CNAME"]}</option>";
 									 }
 								 }
-							
-							
-							?>
-					
+							?>	
 						</select>
-						
-					<br><br>
-					
-					<label>Section</label><br>
-					
-						<select name="sec" required class="input2">
+						<select name="sec" required class="input2" style="width:33%">
 						<?php
 							$sl="select DISTINCT(CSEC) from class";
 							$r=$db->query($sl);
 								if($r->num_rows>0)
 								{
-									echo "<option value=''>Select</option>";
+									echo "<option value=''>Section</option>";
 									while($ro=$r->fetch_assoc())
 									{
 										echo "<option value='{$ro["CSEC"]}'>{$ro["CSEC"]}</option>";
 									}
-								}
-						
-						
-						
-						
+								}						
 						?>
-						
-						
-						</select><br></br>
-						
-						
-						
-					<label>Subject</label><br>
-					
-						<select name="sub" required class="input2">
+						</select>
+						<select name="sub" required class="input2" style="width:33%">
 						<?php
 							$s="select * from sub";
 							$re=$db->query($s);
 							if($re->num_rows>0)
 							{
-								echo "<option value=''>Select</option>";
+								echo "<option value=''>Subject</option>";
 								while($r=$re->fetch_assoc())
 								{
 								echo "<option value='{$r["SNAME"]}'>{$r["SNAME"]}</option>";
@@ -133,17 +94,18 @@
 						?>
 						</select>
 						
-					<br><br>
+					<br>
 					
 					<button type="submit" class="btn" name="submit">Add  Details</button>
 					</form>
 					</div>
 					<div class="rbox1">
-					<h3> Details</h3><br>
+                        <br>
+					<h3> Classes Handling</h3><br>
 						<?php
 						if(isset($_GET["mes"]))
 						{
-							echo"<div class='error'>{$_GET["mes"]}</div>";	
+							echo"<div class='error'>{$_GET["mes"]}<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close' onclick='closeAlert(this)'></button> </div>";	
 						}
 					
 					?>
@@ -156,7 +118,7 @@
 							<th>Delete</th>
 						</tr>
 						<?php
-							$s="select * from hclass";
+							$s="select * from hclass where TID={$_SESSION["TID"]}";
 							$res=$db->query($s);
 							if($res->num_rows>0)
 							{
@@ -176,11 +138,7 @@
 									";
 								}
 							}
-						
-						
-						
-						?>
-						
+						?>				
 						</table>
 					</div>
 				</div>
