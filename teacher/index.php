1f73c44e06de15ca6cd9ffcb6d4d5bc22d8c6552
Chokkalingam.s <chok22017.it@rmkec.ac.in>
2024-06-25 13:13:58 +0530

    staff

--- teacher/index.php
+++ teacher/index.php
@@ -41,12 +41,52 @@
 
         <h3 class="text">Welcome <?php echo $_SESSION["TNAME"]; ?></h3><hr><br>
      
-        <div class="content1">
-          
-                <h4>Profile</h4>
-                <p>Name: <?php echo $row["TNAME"]; ?></p>
-                
-        </div>
+        <div class="content">
+
+              <div class="rbox1">
+                <h3> Profile</h3><br>
+
+                    <table class="table">
+                        <tr><td colspan="2"><img src="<?php echo $row["IMG"] ?>" height="100" width="100" alt="Kindly Upload Your Photo (Scroll)"></td></tr>
+                        <tr><th>Name </th> <td><?php echo $row["TNAME"] ?> </td></tr>
+                        <tr><th>Qualification </th> <td><?php echo $row["QUAL"] ?>  </td></tr>
+                        <tr><th>Salary </th> <td> <?php echo $row["SAL"] ?>  </td></tr>
+                        <tr><th>Phone No </th> <td> <?php echo $row["PNO"] ?> </td></tr>
+                        <tr><th>E - Mail </th> <td> <?php echo $row["MAIL"] ?> </td></tr>
+                        <tr><th>Address </th> <td> <?php echo $row["PADDR"] ?> </td></tr>
+                    </table>
+                </div>
+				
+                <h3>Update Profile</h3><br>
+                <div class="lbox1">
+                <?php
+                    if(isset($_POST["submit"]))
+                    {
+                        $target="../images/staff/";
+                        $target_file=$target.basename($_FILES["img"]["name"]);
+                        
+                        if(move_uploaded_file($_FILES['img']['tmp_name'],$target_file))
+                        {
+                            $sql="update staff set PNO='{$_POST["pno"]}',MAIL='{$_POST["mail"]}',PADDR='{$_POST["addr"]}',IMG='{$target_file}'where TID={$_SESSION["TID"]}";
+                            $db->query($sql);
+                            echo "<div class='success'>Insert Success</div>";
+                        }
+                        
+                    }
+                ?>
+                <form  enctype="multipart/form-data" role="form"  method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+                        <label>  Phone No</label><br>
+                        <input type="text" maxlength="10" required class="input2" name="pno"><br><br>
+                        <label>  E - Mail</label><br>
+                        <input type="email"  class="input2" required name="mail"><br><br>
+                        <label>  Address</label><br>
+                        <textarea rows="5" name="addr" ></textarea><br><br>
+                        <label> Image</label><br>
+                        <input type="file"  class="input2" required name="img"><br><br>
+                    <button type="submit" class="btn" name="submit">Add Profile Details</button>
+                    </form>
+                </div>
+            </div>
        
     </div>
 </body>
